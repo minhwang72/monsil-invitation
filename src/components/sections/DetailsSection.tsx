7f3be0151e8da69e8efa4b9302cfb0c90841d690
hwang min <minhwang7201@naver.com>
2025-07-31 15:16:08 +0900

    Improve parking notice text layout: use single line with proper word wrapping and hanging indent for line breaks

--- src/components/sections/DetailsSection.tsx
+++ src/components/sections/DetailsSection.tsx
@@ -224,8 +224,9 @@ export default function LocationSection() {
                 ※ 교회 방침에 의하여 축하 화환은 정중히 사양합니다.
               </p>
               <div className="text-black text-xs leading-relaxed" style={{ fontFamily: 'MaruBuri, -apple-system, BlinkMacSystemFont, system-ui, sans-serif' }}>
-                <p>※ 주차공간이 협소하니, 가급적 대중교통을 이용해 주시고</p>
-                <p className="pl-4 md:pl-6">덕수궁 돌담길의 정취를 느끼며 오시길 바랍니다.</p>
+                <p className="whitespace-normal break-words parking-notice">
+                  ※ 주차공간이 협소하니, 가급적 대중교통을 이용해 주시고 덕수궁 돌담길의 정취를 느끼며 오시길 바랍니다.
+                </p>
               </div>
             </div>
           </div>
@@ -242,6 +243,13 @@ export default function LocationSection() {
           </div>
         </div>
       )}
+
+      <style jsx>{`
+        .parking-notice {
+          text-indent: -1em;
+          padding-left: 1em;
+        }
+      `}</style>
     </>
   )
 } 
